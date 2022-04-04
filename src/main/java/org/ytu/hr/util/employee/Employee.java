07bb98d45e13f7e2f2b52baf8b6dd3ba98635b8b
Evren Ispiroglu <evrenn.ispiroglu@gmail.com>
2022-04-04 20:09:39 +0300

    Employee initialization

--- src/main/java/org/ytu/hr/util/employee/Employee.java
+++ src/main/java/org/ytu/hr/util/employee/Employee.java
@@ -1,13 +1,48 @@
 package org.ytu.hr.util.employee;
 
+import org.ytu.hr.util.employee.adress.Address;
+import org.ytu.hr.util.employee.candidate.Candidate;
+import org.ytu.hr.util.employee.gender.Gender;
+
 import java.util.Date;
 
-public class Employee {
-    private String m_firstName;
-    private String m_lastName;
-    private Integer m_citizenID;
-    private String m_address;
-    private Date m_bornDate;
-    private String m_gender;
-    
+
+public class Employee extends Candidate {
+    protected final Integer employeeID;
+    protected Integer salary;
+    protected Integer paidLeave;
+    protected Integer absentDay;
+    public Employee(Integer citizenID, Integer phoneNumber, String firstName, String lastName, String email, Gender gender, Address address, Date bornDate, Date applicationDate, Integer employeeID) {
+        super(citizenID, phoneNumber, firstName, lastName, email, gender, address, bornDate, applicationDate);
+        this.employeeID = employeeID;
+    }
+
+    public Integer getEmployeeID() {
+        return employeeID;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Integer getPaidLeave() {
+        return paidLeave;
+    }
+
+    public void setPaidLeave(Integer paidLeave) {
+        this.paidLeave = paidLeave;
+    }
+
+    public Integer getAbsentDay() {
+        return absentDay;
+    }
+
+    public void setAbsentDay(Integer absentDay) {
+        this.absentDay = absentDay;
+    }
+
 }
