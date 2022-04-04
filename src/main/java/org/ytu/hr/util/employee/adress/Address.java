28810e3dd7d15e4d91def0331e60d31d17726457
Evren Ispiroglu <evrenn.ispiroglu@gmail.com>
2022-04-04 20:30:55 +0300

    evren

--- src/main/java/org/ytu/hr/util/employee/adress/Address.java
+++ src/main/java/org/ytu/hr/util/employee/adress/Address.java
@@ -1,10 +1,50 @@
 package org.ytu.hr.util.employee.adress;
 
 public class Address {
-    public String country;
     public Integer zipCode;
+    public String country;
     public String city;
     public String street;
 
+    public Address(){
 
+    }
+    public Address(Integer zipCode, String country, String city, String street) {
+        this.zipCode = zipCode;
+        this.country = country;
+        this.city = city;
+        this.street = street;
+    }
+
+    public Integer getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(Integer zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
 }
