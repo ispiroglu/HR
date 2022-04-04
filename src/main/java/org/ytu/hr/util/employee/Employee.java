f24b6cf34a8ca70f03fe489b3ff8d3914933a4fa
Evren Ispiroglu <evrenn.ispiroglu@gmail.com>
2022-04-04 18:41:02 +0300

    hibernate dependency

---
+++ src/main/java/org/ytu/hr/util/employee/Employee.java
@@ -0,0 +1,13 @@
+package org.ytu.hr.util.employee;
+
+import java.util.Date;
+
+public class Employee {
+    private String m_firstName;
+    private String m_lastName;
+    private Integer m_citizenID;
+    private String m_address;
+    private Date m_bornDate;
+    private String m_gender;
+    
+}
