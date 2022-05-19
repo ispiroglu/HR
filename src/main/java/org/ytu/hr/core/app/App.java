72ccd5658eea0952b7ef1adfbe3e2386d83b4051
emir <cagri.aykin@std.yildiz.edu.tr>
2022-05-19 17:32:39 +0300

    DayOff works fine
    maxPaidLeave days added -> 30
    addPaidLeave and add absent day methods added.
    Detailed implementation required

--- src/main/java/org/ytu/hr/core/app/App.java
+++ src/main/java/org/ytu/hr/core/app/App.java
@@ -3,10 +3,17 @@ package org.ytu.hr.core.app;
 import org.ytu.hr.core.util.db.HibernateUtil;
 import org.ytu.hr.frontend.loginPage.LoginPage;
 
+import static org.ytu.hr.core.dayoff.DayOff.*;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         HibernateUtil.setup();
         LoginPage frame = new LoginPage();
         frame.setVisible(true);
+        Thread.sleep(5000);
+
+        updateEmployee(1,1111 );
+        addAbsentDay(1, 2);
+        addPaidLeave(1,18);
     }
 }
--- src/main/java/org/ytu/hr/core/dayoff/DayOff.java
+++ src/main/java/org/ytu/hr/core/dayoff/DayOff.java
@@ -10,12 +10,40 @@ import java.text.Annotation;
 
 
 public class DayOff {
+    private static final int maxPaidLeave=30;
+
     public static void updateEmployee(Integer EmployeeID, int salary){
         try {
             Session session = HibernateUtil.getSessionFactory().openSession();
             session.getTransaction().begin();
             Employee employee = session.get(Employee.class, EmployeeID);
             employee.setSalary(salary);
+            session.saveOrUpdate(employee);
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void addPaidLeave(Integer EmployeeID, int days){
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.getTransaction().begin();
+            Employee employee = session.get(Employee.class, EmployeeID);
+            if(employee.getPaidLeave()+days <= maxPaidLeave ){
+                employee.setPaidLeave(employee.getPaidLeave()+days);
+            }
+            session.saveOrUpdate(employee);
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void addAbsentDay(Integer EmployeeID, int days){
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.getTransaction().begin();
+            Employee employee = session.get(Employee.class, EmployeeID);
+            employee.setAbsentDay(employee.getAbsentDay()+days);
             session.saveOrUpdate(employee);
             session.getTransaction().commit();
         } catch (HibernateException e) {
