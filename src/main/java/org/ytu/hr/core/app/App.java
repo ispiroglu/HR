9577a7a4d9e993114867bcee8fbefa8e41d0c79e
Mehmet Keçeci <71701959+mehmet-k@users.noreply.github.com>
2022-05-21 00:36:43 +0300

    RecruitEmployee method done

--- src/main/java/org/ytu/hr/core/app/App.java
+++ src/main/java/org/ytu/hr/core/app/App.java
@@ -1,13 +1,23 @@
 package org.ytu.hr.core.app;
 
+import org.ytu.hr.core.recruit.RecruitEmployee;
 import org.ytu.hr.core.util.db.HibernateUtil;
 import org.ytu.hr.frontend.loginPage.LoginPage;
+
+import static org.ytu.hr.core.recruit.RecruitEmployee.RecordEmployeeToDB;
 
 public class App {
     public static void main(String[] args) {
         HibernateUtil.setup();
         LoginPage frame = new LoginPage();
+        frame.setVisible(true);/*
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        RecordEmployeeToDB("3","2","0","0","3","0","11-JUN-2011","25-MAY-2000","0","0");
+*/
 
-        frame.setVisible(true);
     }
 }
--- src/main/java/org/ytu/hr/core/recruit/RecruitEmployee.java
+++ src/main/java/org/ytu/hr/core/recruit/RecruitEmployee.java
@@ -2,48 +2,93 @@ package org.ytu.hr.core.recruit;
 
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.ytu.hr.core.models.employee.Employee;
-import org.ytu.hr.core.util.adress.Address;
-import org.ytu.hr.core.util.gender.Gender;
-import org.ytu.hr.core.util.position.Position;
+import org.ytu.hr.core.util.db.HibernateUtil;
 
-import java.sql.Date;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
+
+
 
 public class RecruitEmployee{
 
-    private SessionFactory sessionFactory;
-    private Session session;
-    Employee e;
+
 
     public RecruitEmployee() {
     }
 
-    public boolean RecordEmployeeToDB(Integer EmployeeID,int CandidateID, int CitizenID, int PhoneNumber, String FirstName,
-                                      String LastName, String eMail, Gender Gender, Date BirthDate, Date ApplicationDate,
-                                      Integer Salary, Address Address, Position Position){
 
-        Employee tmpEmployee;
-        tmpEmployee = session.get(Employee.class,EmployeeID);
-        if (tmpEmployee != null){
-            return false;
-        }/*
-        else{
 
-        }
-*/
-        e = new Employee(CandidateID,CitizenID,PhoneNumber,FirstName,LastName,eMail,Gender, Address,BirthDate,ApplicationDate,EmployeeID);
-        e.setAbsentDay(0);
-        e.setPaidLeave(0);
-        e.setSalary(Salary);
-        e.setPosition(Position);
+    /*
+        IF RecordEmployeeToDB returns 0, Employee already exists!
+                              returns 1, there is an employee with same ID
+                              returns 2, there is an employee with same eMail
+                              returns 3, there is an employee with same PhoneNumber
+                              returns -1, employee added to DB with no problem
+        !!!!UPDATE allEmployees list after this method has called
 
+     */
+
+    public static int RecordEmployeeToDB( String CitizenID, String PhoneNumber, String FirstName,
+                                      String LastName, String eMail, String Gender, String BirthDate, String ApplicationDate,
+                                      String Salary, String Position){
+        Employee e;
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.getTransaction().begin();
+        List<Employee> allEmployees = session.createQuery("select a FROM Employee a", Employee.class).getResultList();
+        String employeeCount = Integer.toString(allEmployees.size());
+         for (Employee a: allEmployees){
+            if(Objects.equals(a.getCitizenID(), Long.parseLong(CitizenID)))
+                return 1;
+            else if(Objects.equals(a.getEmail(), eMail))
+                return 2;
+            else if(  Objects.equals(a.getPhoneNumber(), Long.parseLong(PhoneNumber))){
+                return 3;
+            }
+        }
+
+        try{
+            e = new Employee();
+            e.setEmployeeID(Integer.parseInt(employeeCount));
+            e.setCitizenID(Long.parseLong(CitizenID));
+            e.setPhoneNumber(Long.parseLong(PhoneNumber));
+            e.setFirstName(FirstName);
+            e.setLastName(LastName);
+            e.setEmail(eMail);
+            e.setGender(Gender);
+
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
+           // java.util.Date date = formatter.parse();
+            java.util.Date date = formatter.parse(BirthDate);
+            java.sql.Date tmpDate = new java.sql.Date(date.getTime());
+            e.setBirthDate(tmpDate);
+
+            date = formatter.parse(ApplicationDate);
+            tmpDate =  new java.sql.Date(date.getTime());
+            e.setApplicationDate(tmpDate);
+
+            e.setAbsentDay(0);
+            e.setPaidLeave(0);
+            e.setSalary(Integer.parseInt(Salary));
+            e.setPosition(Position);
 
 
-        return true;
+
+
+            session.saveOrUpdate(e);
+            session.getTransaction().commit();
+
+        }
+        catch (java.text.ParseException exception){
+            System.out.println("Data format not valid, valid format:dd-MMM-yyyy \n");
+            exception.printStackTrace();
+        }
+
+
+        return -1;
     }
 
 
