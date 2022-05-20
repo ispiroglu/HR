bbec58cbc4921882a1f193b809945b7296972e86
Emir Çağrı Aykın <73666408+emiraykin@users.noreply.github.com>
2022-05-20 23:26:10 +0300

    .

--- src/main/java/org/ytu/hr/core/app/App.java
+++ src/main/java/org/ytu/hr/core/app/App.java
@@ -4,7 +4,7 @@ import org.ytu.hr.core.util.db.HibernateUtil;
 import org.ytu.hr.frontend.loginPage.LoginPage;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         HibernateUtil.setup();
         LoginPage frame = new LoginPage();
         frame.setVisible(true);
