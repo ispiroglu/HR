dc469be7ddbbfe5373845546673f3e1c3bd2c44a
Gürol Berkay Çınar <gurolberkaycinar@gmail.com>
2022-05-22 16:27:58 +0300

    Fixes
    Deleted excess declarations and updated .gitignore

--- src/main/java/org/ytu/hr/frontend/addEmployeePage/AddEmployePage.java
+++ src/main/java/org/ytu/hr/frontend/addEmployeePage/AddEmployePage.java
@@ -32,9 +32,6 @@ public class AddEmployePage extends javax.swing.JFrame {
         setLocation(size.width/2 - getWidth()/2, size.height/2 - getHeight()/2);
         dogumTarihiTextField.setText("GG-AA-YYYY");
         ilComboBox.setSelectedItem("Adana");
-        Toolkit toolkit = getToolkit();
-        Dimension size = toolkit.getScreenSize();
-        setLocation(size.width/2 - getWidth()/2, size.height/2 - getHeight()/2);
     }
 
     /**
